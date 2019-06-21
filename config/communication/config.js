31126a6ee323681dae987988751cdc04527321ed
Esteban <eechava6@eafit.edu.co>
2019-06-20 22:57:11 -0500

    Cloud conf hot fix

--- config/communication/config.js
+++ config/communication/config.js
@@ -2,5 +2,5 @@ var host = process.env.kafka+":9092"
 module.exports = {
     kafka_products_topic: 'products',
     kafka_clients_topic: 'clients',
-    kafkaHost: host,
+    kafkaHost: '172.31.38.252:9092',
 }; 
